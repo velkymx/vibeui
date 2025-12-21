3dda84a33c31a0a3c61f57ffaa08b656519313be
Alan B <ajb@wow.com>
2025-12-21 10:16:33 -0800

    Fix TypeScript declaration generation

---
+++ src/components/VibeContainer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed, type PropType } from 'vue'
+import type { Tag, ContainerType } from '../types'
+
+const props = defineProps({
+  fluid: { type: [Boolean, String] as PropType<boolean | ContainerType>, default: false },
+  tag: { type: String as PropType<Tag>, default: 'div' }
+})
+
+const emit = defineEmits(['component-error'])
+
+const containerClass = computed(() => {
+  if (props.fluid === false) {
+    return 'container'
+  } else if (props.fluid === true) {
+    return 'container-fluid'
+  } else {
+    return `container-${props.fluid}`
+  }
+})
+</script>
+
+<template>
+  <component :is="tag" :class="containerClass">
+    <slot />
+  </component>
+</template>
